dc6dbc9381dde8beefac27cf5c14e5bf960a9de4
tomgo5 <tomgoldstn@gmail.com>
2025-04-03 11:25:03 +0300

    changed methods order and resolved warnings

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 
-    public static void method1(String[] args) {
+    public static void method1() {
         System.out.println("Another Hello World!");
     }
 
-    public static void method2(String[] args) {
+    public static void method2() {
         System.out.println("TEST");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        method1();
+        method2();
     }
 }
